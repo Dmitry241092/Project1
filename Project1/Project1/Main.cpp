dca3dfa984058a60fa492d244505e15882d3ec2f
Dmitry241092 <156231677+Dmitry241092@users.noreply.github.com>
2024-02-23 14:39:37 +0300

    Add files via upload

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <string>
 
-int main()
+void func_for(bool c, int n) {
+	for (int i = 0; i <= n; i++)
+	{
+		if (i % 2 == c)
+			std::cout << i << " ";
+	}
+}
+
+void foo(bool c, int n) {
+	func_for(c, n);
+}
+int main(int argc, char* argv[])
 {
-	std::string first = "Hello", second = " World";
-	std::string final = first + second;
-	std::cout << final << "\n";
-	std::cout << final.length() << "\n";
-	std::string::size_type pos = final.find("Hello World");
-	first = final.substr(0);
-	second = final.substr(5);
-	std::cout << first[0] << "\n";
-	std::cout << second[5];
+    bool c; int x;
+    setlocale(0, "Russian");
+    std::cout << "¬ведите число:";
+    std::cin >> x;
+    std::cout << "¬ведите '1' дл€ отображени€ нечетных чисел, либо - '0' дл€ отображени€ четных:";
+    std::cin >> c;
+
+    foo(c, x);
+
+
+    return 0;
 }
