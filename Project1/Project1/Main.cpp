5bb562cfcd6f7c84935c45d0e2ed30fd5b713306
Dmitry241092 <156231677+Dmitry241092@users.noreply.github.com>
2024-03-01 20:29:38 +0300

    Add files via upload

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,28 +1,42 @@
-#include <iostream> 
-#include <cmath> 
+#include <iostream>  
+#include <algorithm> 
+#include <cstdlib>
+using namespace std;
 
-class Vector {
-private:
-    double x;
-    double y;
-    double z;
-
+class player
+{
+	string name;
+	int points;
 public:
-    Vector(double x, double y, double z) : x(x), y(y), z(z) {}
-
-    void print() {
-        std::cout << "Vector(" << x << ", " << y << ", " << z << ")" << std::endl;
-    }
-
-    double length() {
-        return std::sqrt(x * x + y * y + z * z);
-    }
+	friend bool point_sorter(const player&, const player&);
+	string get_name()const { return name; }
+	int get_points()const { return points; }
+	void set_name(string n) { name = n; }
+	void set_points(int n) { points = n; }
 };
-
+bool point_sorter(const player& l, const player& r)
+{
+	return l.points > r.points;
+}
 int main() {
-    Vector v(1.0, 2.0, 3.0);
-    v.print();
-    std::cout << "Length: " << v.length() << std::endl;
-
-    return 0;
+	system("chcp 1251 > nul");  
+	size_t n;
+	cout << "¬ведите число игроков";
+	cin >> n;
+	player* players = new player[n];
+	for (size_t i = 0; i < n; i++)
+	{
+		string name;
+		int point;
+		cout << "¬ведите данные игрока " << i << " (им€ очки):";
+		cin >> name >> point;
+		players[i].set_name(name);
+		players[i].set_points(point);
+	}
+	sort(players, players + n, point_sorter);
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << players[i].get_name() << " " << players[i].get_points() << '\n';
+	}
+	delete[] players;
 }
